f91ed397811d3f2e2969f7d6b91d2bf1d3a16d95
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-10-22 21:41:03 -0300

    feat: update portfolio section with BlurText, colored text, and icon images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,12 @@
-'use client'
+"use client"
 
-import ComingSoon from '@/components/layout/ComingSoon'
-import LightRays from '../components/LightRays'
-import { useEffect, useState } from 'react'
+import Portfolio from '@/components/layout/Portfolio';
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setLoaded(true), 700)
-    return () => clearTimeout(timer)
-  }, []);
 
   return (
     <div className="relative flex items-center justify-center h-screen">
-      <div className="absolute inset-0 z-0">
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#c1121f"
-          raysSpeed={1.5}
-          lightSpread={1}
-          rayLength={15}
-          followMouse={true}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={0.05}
-          className="custom-rays"
-        />
-      </div>
-
-      <div
-        className={`relative z-10 flex items-center justify-center h-full transition-opacity duration-700 ${loaded ? 'opacity-100' : 'opacity-0'
-          }`}
-      >
-        <ComingSoon />
-      </div>
+      <Portfolio />
     </div>
   )
 }
---
+++ src/components/layout/Portfolio.tsx
@@ -0,0 +1,89 @@
+import BlurText from "../BlurText.jsx"
+import FadeContent from "../FadeContent.jsx"
+import Image from "next/image.js"
+
+import "../../styles/variables.css"
+import "../../styles/portfolio.css"
+
+const Portfolio = () => {
+    return (
+        <div className="flex flex-col h-screen w-[90%]">
+
+            {/* top div */}
+            <div className="mt-7 flex flex-row justify-between">
+                <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                    <Image src="/icon.png" width={55} height={55} alt={"teste"} />
+                </FadeContent>
+
+                <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                    <Image src="/icon.png" width={55} height={55} alt={"teste"} />
+                </FadeContent>
+            </div>
+
+            {/* middle div */}
+            <div className="flex flex-col justify-center items-center h-screen">
+                <div className="flex flex-row items-center justify-center mb-3 gap-3">
+                    <div className="dot"></div>
+                    <p className="text-1xl text-gray font-medium font-instrument">DISPONÍVEL PARA TRABALHO</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <BlurText
+                        text="MICHAELLE"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-8xl text-letter font-instrument font-extrabold -mb-3"
+                        animationFrom={undefined}
+                        animationTo={undefined}
+                        onAnimationComplete={undefined} />
+                    <BlurText
+                        text="OLIVEIRA"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-8xl mb-8 text-letter font-instrument font-extrabold"
+                        animationFrom={undefined}
+                        animationTo={undefined}
+                        onAnimationComplete={undefined} />
+                </div>
+            </div>
+
+            {/* bottom div */}
+            <div className="flex flex-row justify-between">
+                <BlurText
+                    text="DESENVOLVEDORA"
+                    delay={150}
+                    animateBy="words"
+                    direction="top"
+                    className="text-[1.2rem] mb-8 text-letter font-instrument font-normal"
+                    animationFrom={undefined}
+                    animationTo={undefined}
+                    onAnimationComplete={undefined} />
+                <div className="flex flex-row">
+                    <BlurText
+                        text="FRONTEND"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-[1.2rem] mb-8 text-letter font-instrument font-normal"
+                        animationFrom={undefined}
+                        animationTo={undefined}
+                        onAnimationComplete={undefined} />
+
+                    <BlurText
+                        text=" + FULLSTACK"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-[1.2rem] mb-8 text-highlight font-instrument font-bold"
+                        animationFrom={undefined}
+                        animationTo={undefined}
+                        onAnimationComplete={undefined} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Portfolio
