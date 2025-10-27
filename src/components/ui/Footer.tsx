36bb82c3a1a24a8e23c60b6ef9bddef79912da0c
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-10-27 20:32:40 -0300

    feat: update Footer component with contact information and social media icons

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faDiscord, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const teste = () => {
     return (
-        <div className='bg-background-footer w-full h-20 flex items-center justify-center'>
-            <p className='text-amber-50'>teste</p>
+        <div className='bg-background-footer w-full h-20 mt-10 flex items-center justify-center font-dm-sans'>
+            <div>
+                <div className='flex flex-row justify-between'>
+                    <a href="mailto:michaelle.oliveira101103@gmail.com" className='flex justify-center items-center gap-2'>
+                        <FontAwesomeIcon icon={faEnvelope} color='#fefefd' />
+                        <p className='text-letter'>michaelle.oliveira101103@gmail.com</p>
+                    </a>
+
+                    <div>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        <FontAwesomeIcon icon={faGithub} />
+                        <FontAwesomeIcon icon={faDiscord} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
