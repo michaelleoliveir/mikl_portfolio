fe1926640e4a27d3df0293a268acf140ac27e77a
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-10-29 21:12:49 -0300

    feat: import AOS CSS into the globals.css file and remove the import from AosWrapper.

--- src/components/ui/AosWrapper.tsx
+++ src/components/ui/AosWrapper.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, PropsWithChildren } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 const AosWrapper = ({ children }: PropsWithChildren) => {
     useEffect(() => {
