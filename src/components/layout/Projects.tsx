19420b7d789c0a2158fb396301bcabb0cc7c68bf
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-11-21 14:18:26 -0300

    feat: finishing projet component and adding tags section

--- src/components/layout/Projects.tsx
+++ src/components/layout/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
             title: "CRUD Laravel",
             link: "https://github.com/michaelleoliveir/crud-laravel",
             subtitle: "Aplicação CRUD desenvolvida em Laravel, permitindo criar, visualizar, editar, deletar e pesquisar publicações por meio de uma interface simples e intuitiva.",
-            tags: ['Laravel', 'PHP', 'HTML', 'CSS']
+            tags: ['Laravel', 'PHP', 'Blade', 'HTML', 'CSS', 'Bootstrap']
         },
         {
             id: 2,
@@ -17,7 +17,7 @@ const Projects = () => {
             title: "Union Developers Clone",
             link: "https://github.com/michaelleoliveir/uniondevelopers-clone",
             subtitle: "Clone do site da Union Developers, reproduzido com foco em layout, responsividade e fidelidade visual ao original.",
-            tags: []
+            tags: ['NextJS', 'TypeScript', 'Tailwind', 'Lucide', 'Zod']
         },
         {
             id: 3,
@@ -25,7 +25,7 @@ const Projects = () => {
             title: "User Management",
             link: "https://github.com/michaelleoliveir/user-management",
             subtitle: "Sistema de gerenciamento de usuários para registrar, atualizar e excluir usuários. Este projeto é ideal para quem busca implementar funcionalidades básicas de CRUD (Create, Read, Update, Delete) em uma aplicação web.",
-            tags: []
+            tags: ['React', 'JavaScript', 'Shadcn', 'Express', 'MongoDB', 'Cypress', 'Jest']
         },
         {
             id: 4,
@@ -33,7 +33,7 @@ const Projects = () => {
             title: "Ascone Landing Page",
             link: "https://github.com/michaelleoliveir/ascone-landing-page",
             subtitle: "Clone da landing page da Ascone Finance, seguindo fielmente o conceito visual encontrado no Dribbble e mantendo a estrutura, identidade e proposta original do design.",
-            tags: []
+            tags: ['React', 'JavaScript', 'Tailwind', 'Shadcn']
         }
     ]
 
--- src/components/ui/Project.tsx
+++ src/components/ui/Project.tsx
@@ -8,7 +8,7 @@ interface ProjectProps {
     tags: string[]
 }
 
-const Project: React.FC<ProjectProps> = ({ image, title, link, subtitle }) => {
+const Project: React.FC<ProjectProps> = ({ image, title, link, subtitle, tags }) => {
     return (
         <div className="font-instrument w-full flex flex-col items-center mb-17">
             
@@ -25,9 +25,20 @@ const Project: React.FC<ProjectProps> = ({ image, title, link, subtitle }) => {
                 
             </div>
 
-            <div className="w-[80%]">
-                <h1 className="text-letter text-3xl font-bold">{title}</h1>
-                <p className="text-gray text-[1.1rem]">{subtitle}</p>
+            <div className="w-[80%] flex flex-col">
+                <div className="mb-5 flex flex-wrap flex-row">
+                    {tags.map((tag, index) => {
+                        return (
+                            <span key={index} className="text-neon text-[0.9rem] w-[10%] mr-3 px-3 py-1 text-center rounded-4xl bg-highlight-darker/60">
+                                {tag}
+                            </span>
+                        )
+                    })}
+                </div>
+                <div>
+                    <h1 className="text-letter text-3xl font-bold">{title}</h1>
+                    <p className="text-gray text-[1.1rem]">{subtitle}</p>
+                </div>
             </div>
         </div>
     );
